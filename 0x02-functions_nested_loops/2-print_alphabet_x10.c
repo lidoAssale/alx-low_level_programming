7237bbc0c7322094507547975de4349eb2448480
lidoAssale <lido.assale@gmail.com>
2022-07-06 07:11:32 -0700

    print alphabet 10 times script

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main: print_alphabet_x10 - print lowercase alphabet
+ * Return: 0 always (succes)
+ */
+void print_alphabet_x10(void)
+{
+int a, i;
+for (a = 0; a < 9; a++)
+{
+for (i = 97; i<= 122; i++)
+{
+_putchar(i);
+}
+}
+}
