3249f26838a1c4c380307a71d0c572fa7a06b762
lidoAssale <lido.assale@gmail.com>
2022-07-06 19:29:01 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-long int i, j, k , next;
+long int i, j, k, next;
 j = 1;
 k = 2;
 for (i = 1; i <= 50; i++)
@@ -15,7 +15,8 @@ if (j != 20365011074)
 {
 printf("%ld, ", j);
 }
-else {
+else
+{
 printf("%ld\n", j);
 }
 next = j + k;
